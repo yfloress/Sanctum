7f08c5510cba992fbcc861919eb8c7a2c2603e5d
yfloress <109782222+yfloress@users.noreply.github.com>
2025-11-30 14:58:00 -0300

    chore(deps): install recharts library

--- flake.nix
+++ flake.nix
@@ -37,8 +37,7 @@
           libsoup_3
           webkitgtk_4_1
           librsvg
-          deno        
-          imagemagick
+          deno
         ];
       in
       {
@@ -53,7 +52,7 @@
           shellHook = ''
             export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
             export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-            
+
             export PKG_CONFIG_PATH=${pkgs.openssl_3.dev}/lib/pkgconfig:$PKG_CONFIG_PATH
 
             echo "🛡️ SANCTUM DEV SHELL ACTIVO"
