b3c4635a7d2319bb61613822e11c902975dda248
yfloress <109782222+yfloress@users.noreply.github.com>
2025-12-29 22:17:09 -0300

    fix(settings): disable proxy field when off

--- src/ui/callbacks/settings.rs
+++ src/ui/callbacks/settings.rs
@@ -91,11 +91,6 @@ pub fn setup_settings_callbacks<N>(
 
                     if let Err(err) = controller.validate_crypto_proxy_url(current_url) {
                         notify(err.to_string(), true);
-                        let _ = controller.set_crypto_proxy_enabled(false);
-                        if let Some(ui) = ui_weak.upgrade() {
-                            ui.global::<SettingsAdapter>().set_proxy_enabled(false);
-                        }
-                        return;
                     }
                 }
 
