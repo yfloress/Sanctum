8ce985bb1de246b503e724af394a2eb0007a63b6
yfloress <109782222+yfloress@users.noreply.github.com>
2026-01-23 00:14:58 +0000

    style(web): refresh icons and toggles

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,14 +28,10 @@ export default defineConfig({
         scope: base,
         icons: [
           {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: 'pwa.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any',
           },
         ],
       },
